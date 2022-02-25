294999759c3058d7228044458913cac62205b685
jeaseung <kingply2@naver.com>
2022-02-25 09:56:07 +0900

    서버 소스

---
+++ main.cpp
@@ -0,0 +1,106 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include<WinSock2.h>
+#include<iostream>
+#include <string>
+
+using namespace std;
+
+
+#pragma comment(lib, "ws2_32.lib")
+
+struct NumberPacket {
+	int Number1;
+	int Number2;
+};
+
+int main()
+{
+	WSAData wsaData;
+
+	//Winsock 초기화 
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData))
+	{
+		cout << "Winsock Error : " << GetLastError() << endl;
+		exit(-1);
+	}
+
+	//2. create socket
+	SOCKET ServerSocket;
+
+	ServerSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (ServerSocket == INVALID_SOCKET)
+	{
+		cout << "socket Error : " << GetLastError() << endl;
+		exit(-1);
+	}
+
+	//3. ip, port를 할당하여 소켓이랑 연결
+	SOCKADDR_IN ServerAddr;
+
+	memset(&ServerAddr, 0, sizeof(ServerAddr));
+	ServerAddr.sin_family = AF_INET;
+	ServerAddr.sin_port = htons(50000);
+	ServerAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if (bind(ServerSocket, (SOCKADDR*)&ServerAddr, sizeof(ServerAddr)) == SOCKET_ERROR)
+	{
+		cout << "bind Error : " << GetLastError() << endl;
+		exit(-1);
+	}
+
+	//4. 클라이언트 연결 대기
+	if (listen(ServerSocket, 0) == SOCKET_ERROR)
+	{
+		cout << "listen Error : " << GetLastError() << endl;
+		exit(-1);
+	}
+
+	while (1)
+	{
+		//5. 클라이언트와 연결
+		SOCKADDR_IN ClientAddr;
+		int ClinetAddrSize = sizeof(ClientAddr);
+		SOCKET ClientSocket;
+		ClientSocket = accept(ServerSocket, (SOCKADDR*)&ClientAddr, &ClinetAddrSize);
+		if (ClientSocket == SOCKET_ERROR)
+		{
+			cout << "accept Error : " << GetLastError() << endl;
+			exit(-1);
+		}
+		//inet network to address
+		cout << "connect ip : " << inet_ntoa(ClientAddr.sin_addr) << endl;
+		//network to host short
+		cout << "connect port : " << ntohs(ClientAddr.sin_port) << endl;
+
+		//6 자료를 클라이언트에게 전송
+		char Buffer[1024];
+		recv(ClientSocket, Buffer, 1024, 0);
+
+		string Numbers = Buffer;
+		string Number1 = Numbers.substr(0, 2);
+		string Number2 = Numbers.substr(3, 2);
+
+		cout << "client sended : " << Number1 << endl;
+		cout << "client sended : " << Number2 << endl;
+
+		int IntResult = stoi(Number1) + stoi(Number2);
+
+		string Result = to_string(IntResult);
+
+		send(ClientSocket, Result.c_str(), Result.length() + 1, 0);
+
+		cout << "client send : " << Result << endl;
+
+		//6. 연결 종료
+		closesocket(ClientSocket);
+	}
+
+	closesocket(ServerSocket);
+
+	//7.Winsock 종료
+	WSACleanup();
+
+	return 0;
+}
